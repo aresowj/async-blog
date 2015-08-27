e212c52a6b82fd224376e4099209cf7b495044aa
Ares Ou <aresowj@gmail.com>
2015-08-26 22:00:37 -0700

    Add config files
    When implementing, specified settings can be overrode by modifying
    config_override.py

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#coding = utf-8
+__author__ = 'aresowj'
+
+'''
+config_default.py
+Using dict to store settings.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'kagami',
+        'database': 'aresou'
+    },
+    'session': {
+        'secret': 'AresOu'
+    }
+}
