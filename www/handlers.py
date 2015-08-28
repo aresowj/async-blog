41061e65b5451b2b9c5a3834757b9c746d6aa25b
Ares Ou <aresowj@gmail.com>
2015-08-28 14:58:32 -0700

    Replace all tabs to 4 spaces

--- www/handlers.py
+++ www/handlers.py
@@ -11,25 +11,25 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 def index(request):
-	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-	blogs = [
-		Blog(id='1', name='Test Blog', summary=summary, created_time=time.time()-120),
-		Blog(id='2', name='Something New', summary=summary, created_time=time.time()-3600),
-		Blog(id='3', name='Learn Swift', summary=summary, created_time=time.time()-7200)
-	]
-	return {
-		'__template__': 'blogs.html',
-		'blogs': blogs
-	}
-	
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_time=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_time=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_time=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
+    
 @get('/api/users')
 def api_get_users(*, page='1'):
-	'''page_index = get_page_index(page)
-	num = yield from User.findNumber('count(id)')
-	p = Page(num, page_index)
-	if num == 0:
-		return dict(page=p, users=())'''
-	users = yield from User.findAll(orderBy='created_time desc')	#, limit=(p.offset, p.limit)
-	for u in users:
-		u.password = '******'
-	return dict(users=users)	#page=p, 
+    '''page_index = get_page_index(page)
+    num = yield from User.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, users=())'''
+    users = yield from User.findAll(orderBy='created_time desc')    #, limit=(p.offset, p.limit)
+    for u in users:
+        u.password = '******'
+    return dict(users=users)    #page=p, 
--- www/models.py
+++ www/models.py
@@ -10,39 +10,39 @@ import time, uuid
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
 def next_id():
-	#Generate random ID with time (15 digits) and uuid4 (random hex)
-	return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+    #Generate random ID with time (15 digits) and uuid4 (random hex)
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
-	__table__ = 'users'
-	
-	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')	#Passing next_id for future initiation.
-	email = StringField(ddl='varchar(50)')
-	password = StringField(ddl='varchar(50)')
-	admin = BooleanField()
-	name = StringField(ddl='varchar(50)')
-	image = StringField(ddl='varchar(500)')
-	created_time = FloatField(default=time.time)
-	
+    __table__ = 'users'
+    
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')    #Passing next_id for future initiation.
+    email = StringField(ddl='varchar(50)')
+    password = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_time = FloatField(default=time.time)
+    
 class Blog(Model):
-	__table__ = 'blogs'
-	
-	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-	user_id = StringField(ddl='varchar(50)')
-	user_name = StringField(ddl='varchar(50)')
-	user_image = StringField(ddl='varchar(500)')
-	title = StringField(ddl='varchar(50)')
-	summary = StringField(ddl='varchar(200)')
-	context = TextField()
-	created_time = FloatField(default=time.time)
-	
+    __table__ = 'blogs'
+    
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    title = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    context = TextField()
+    created_time = FloatField(default=time.time)
+    
 class Comment(Model):
-	__table__ = 'comments'
-	
-	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-	blog_id = StringField(ddl='varchar(50)')
-	user_id = StringField(ddl='varchar(50)')
-	user_name = StringField(ddl='varchar(50)')
-	user_image = StringField(ddl='varchar(500)')
-	context = TextField()
-	created_time = FloatField(default=time.time)
+    __table__ = 'comments'
+    
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    context = TextField()
+    created_time = FloatField(default=time.time)
